880142d26a9d70d04249ef0096e090a79c032477
Kmarchan <kmarchan@student.wethinkcode.co.za>
2018-07-25 13:01:05 +0200

    attempting to normalise

---
+++ sort_int_tab.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_int_tab.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmarchan <kmarchan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/25 10:05:26 by kmarchan          #+#    #+#             */
+/*   Updated: 2018/07/25 10:12:09 by kmarchan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	sort_int_tab(int **tab, size_t n)
+{
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	while (i < n)
+	{
+		j = 0;
+		while (j < n - 1)
+		{
+			if (tab[j + 1] < tab[j])
+			{
+				ft_swap(tab[j + 1], tab[j]);
+			}
+			j++;
+		}
+		i++;
+	}
+}
